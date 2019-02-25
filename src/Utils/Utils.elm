dc95b57bea9407c419f4c834fb82601e60ff7f3e
Mark Boss <mark.boss@uni-tuebingen.de>
2019-02-25 09:56:46 +0100

    Wrong error code for auto refresh login

--- src/Utils/Utils.elm
+++ src/Utils/Utils.elm
@@ -13,8 +13,11 @@ perform =
 
 handleLogoutErrors : model -> SharedState -> (Http.Error -> (model, Cmd msg, SharedStateUpdate)) -> Http.Error -> (model, Cmd msg, SharedStateUpdate)
 handleLogoutErrors model sharedState handler err =
+    let
+        _ = Debug.log "Received error code" err
+    in
     case err of
-        Http.BadStatus 403 -> 
+        Http.BadStatus 401 -> 
             case sharedState.userMail of
                 Just _ -> (model, Cmd.none, RefreshLogin)
                 Nothing -> (model, pushUrl sharedState.navKey (reverseRoute LoginRoute), NoUpdate)
