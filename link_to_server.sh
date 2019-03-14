e9ae8c52719dfc074ab2712e72dcd0ee890e6128
Mark Boss <mark.boss@uni-tuebingen.de>
2019-03-14 18:16:10 +0100

    Unnecessary echo

--- link_to_server.sh
+++ link_to_server.sh
@@ -8,8 +8,6 @@ fi
 STATIC_PATH=$1/static
 MY_PATH="`dirname \"$0\"`"
 
-echo $MY_PATH
-
 ln -s $MY_PATH/index.html $STATIC_PATH/index.html
 ln -s $MY_PATH/elm.js $STATIC_PATH/elm.js
 ln -s $MY_PATH/assets $STATIC_PATH
